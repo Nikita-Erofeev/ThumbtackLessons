3a55e9f8d0a939a6b3abfe117f640043a5b032f8
Step <erofeev.99@mail.ru>
2019-11-01 12:39:17 +0400

    new

---
+++ src/main/java/net/thumbtack/school/introduction/FirstSteps.java
@@ -0,0 +1,11 @@
+package net.thumbtack.school.introduction;
+
+public class FirstSteps {
+    public int sum(int x, int y){
+        return x+y;
+    }
+    public int mul(int x, int y){
+        return x*y;
+    }
+
+}
