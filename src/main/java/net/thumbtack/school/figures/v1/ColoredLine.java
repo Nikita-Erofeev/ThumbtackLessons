05b161d70d0af8e04c7c3184c398f48294d6bebc
Step <erofeev.99@mail.ru>
2019-11-04 20:09:25 +0400

    ...

---
+++ src/main/java/net/thumbtack/school/figures/v1/ColoredLine.java
@@ -0,0 +1,53 @@
+package net.thumbtack.school.figures.v1;
+
+public class ColoredLine extends Line {
+    private int color;
+    public ColoredLine(Point startPoint, Point endPoint, int color){
+        super(startPoint, endPoint);
+        this.color=color;
+    }
+    public ColoredLine(int xLeft, int yTop, int xRight, int yBottom, int color){
+        super(xLeft, yTop, xRight, yBottom);
+        this.color=color;
+    }
+    public ColoredLine(Point endPoint, int color){
+        super(endPoint);
+        this.color=color;
+    }
+    public ColoredLine(int x, int y, int color){
+        super(x, y);
+        this.color=color;
+    }
+    public ColoredLine(int color){
+        super();
+        this.color=color;
+    }
+    public ColoredLine(){
+        super();
+        this.color=1;
+    }
+    public  void setColor(int color){
+        this.color=color;
+    }
+    public int getColor(){
+        return color;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        ColoredLine that = (ColoredLine) o;
+
+        return color == that.color;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = super.hashCode();
+        result = 31 * result + color;
+        return result;
+    }
+}
